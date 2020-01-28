537c0651a49c486c46aeb3fb5f8670a5d99a0381
kushansharma <kushansharma654321@gmail.com>
2020-01-28 22:28:50 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -11,12 +12,13 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mybrary', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log("Connected to mongoose"))
-
+db.once('open', () => console.log("Connected to Mongoose"))
+//'mongodb://localhost/mybrary' || 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
